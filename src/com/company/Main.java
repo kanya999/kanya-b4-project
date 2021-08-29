63c8b07f4a84a4b94547f1cbdcee0325c9bb730d
kanya999 <kanya99cpa@gmail.com>
2021-08-29 09:55:03 -0400

    day2 new modified

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Kanya's project");
     }
+
+    public static String revStr(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    // git status
+    // git add <the file we change> - put into staging area
+    // git commit -m "your msg"
+    // git push
 }
